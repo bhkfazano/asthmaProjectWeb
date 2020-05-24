f7748148529ac9ce2c263a000f57254a9582449a
Bruno Koga <bhkfazano@gmail.com>
2020-05-24 13:16:38 -0300

    implemented validate code to get token screen

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,11 +3,13 @@ import ProfessionalDashboard from './lib/ProfessionalDashboard';
 import SideMenu from './lib/SideMenu';
 import AddPatient from './lib/AddPatient';
 import ManageTeam from './lib/ManageTeam';
+import FitbitSync from './lib/FitbitSync';
 
 export {
     Login,
     ProfessionalDashboard,
     SideMenu, 
     AddPatient,
-    ManageTeam
+    ManageTeam,
+    FitbitSync
 };
