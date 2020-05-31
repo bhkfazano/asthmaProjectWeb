375facae57d66220728dbc010466b00abb87fc44
Bruno Koga <bhkfazano@gmail.com>
2020-05-31 16:45:59 -0300

    save

---
+++ build/precache-manifest.756a93f1a8aa6da11914542e8c6b7603.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d89044d44d752144f33146dc0e3dc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d155eab58c2a0e697295",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "068077c115359b623ad5",
+    "url": "/static/css/main.af72afc9.chunk.css"
+  },
+  {
+    "revision": "d155eab58c2a0e697295",
+    "url": "/static/js/2.62efb337.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.62efb337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068077c115359b623ad5",
+    "url": "/static/js/main.81b87452.chunk.js"
+  },
+  {
+    "revision": "ee596fbc6d1c7d8497c8",
+    "url": "/static/js/runtime-main.d3e5abfb.js"
+  }
+]);
